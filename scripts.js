e14096da6ee0135095502038b2df62868fac02a7
Arthur Hülle Leite <arthurleitee@gmail.com>
2022-04-10 02:18:30 -0300

    white mode and fixing some bugs

--- scripts.js
+++ scripts.js
@@ -33,3 +33,45 @@ $(document).ready(function () {
     once: true,
   });
 });
+
+const html = document.querySelector("html")
+
+const getStyle = (element, style) =>
+window.getComputedStyle(element).getPropertyValue(style)
+
+const initialColors = {
+  colorbackground: getStyle(html, "--colorbackground"),
+  colorbackgrounddark: getStyle(html, "--colorbackgrounddark"),
+  descriptiontext: getStyle(html, "--descriptiontext"),
+  colorcard: getStyle(html, "--colorcard"),
+}
+
+const whiteMode = {
+  colorbackground: "#ced4da",
+  colorbackgrounddark: "#adb5bd",
+  descriptiontext: "#24292f",
+  colorcard: "#adb5bd",
+}
+
+const transformKey = key => "--" + key.replace(/()/, "$1").toLowerCase()
+
+const changeColors = (colors) => {
+Object.keys(colors).map(key => 
+  html.style.setProperty(transformKey(key), colors[key])
+  )
+}
+
+const checkbox = document.querySelector('input[name=theme]')
+
+const checkboxColorMode = JSON.parse(localStorage.getItem('color-mode'))
+
+if (checkboxColorMode) {
+  checkbox.checked = checkboxColorMode
+  changeColors(whiteMode)
+}
+
+checkbox.addEventListener('change', ({ target }) => {
+  target.checked ? changeColors(whiteMode) : changeColors(initialColors)
+  
+  localStorage.setItem('color-mode', target.checked)
+})
